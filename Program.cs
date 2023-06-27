751c801cc0540c9fb664a31c9714f94fac52dd7c
__Ashish <118758755+ashishchougale09@users.noreply.github.com>
2023-06-27 18:54:43 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class ReverseString
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a string: ");
+        string input = Console.ReadLine();
+
+        string reversed = ReverseStringInput(input);
+
+        Console.WriteLine("Reversed string: " + reversed);
+        
+    } 
+
+    public static string ReverseStringInput(string input)
+    {
+        char[] charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+
+    }
+} 
